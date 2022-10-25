4259c1f48c1c237bf8ae7b7afea5c6f9b125f68f
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2022-10-25 21:26:27 +0300

    Added .NET 7 project

--- G-Shop.Application/FormMain.cs
+++ G-Shop.Application/FormMain.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace G_Shop
+namespace G_Shop.Application
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
         }
