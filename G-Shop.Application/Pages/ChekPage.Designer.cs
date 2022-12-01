7944ae144a257837bda75f8af041f068e433aea6
Asya Gorbatchova <gorbatchovaasa123@gmail.com>
2022-12-02 01:17:57 +0200

    Display sale to SalePage

--- G-Shop.Application/Pages/ChekPage.Designer.cs
+++ G-Shop.Application/Pages/ChekPage.Designer.cs
@@ -28,18 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
             this.SuspendLayout();
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Name = "ReportViewer";
+            this.reportViewer.ServerReport.BearerToken = null;
+            this.reportViewer.Size = new System.Drawing.Size(697, 309);
+            this.reportViewer.TabIndex = 0;
             // 
             // ChekPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.reportViewer);
             this.Name = "ChekPage";
-            this.Size = new System.Drawing.Size(900, 614);
+            this.Size = new System.Drawing.Size(697, 309);
+            this.Load += new System.EventHandler(this.ChekPage_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
     }
 }
--- G-Shop.Application/Pages/ChekPage.cs
+++ G-Shop.Application/Pages/ChekPage.cs
@@ -1,15 +1,27 @@
-﻿namespace G_Shop.Application.Pages
+﻿using G_Shop.Application.Reports;
+using G_Shop.Application.Reports.SalesReportDataSetTableAdapters;
+using Microsoft.Reporting.WinForms;
+
+namespace G_Shop.Application.Pages;
+
+public partial class ChekPage : UserControl
 {
-    public partial class ChekPage : UserControl
+    public ChekPage()
     {
-        public ChekPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void buttonDeleteSetSale_Click(object sender, EventArgs e)
-        {
+    private void ChekPage_Load(object sender, EventArgs e)
+    {
+        //reportViewer.LocalReport.ReportEmbeddedResource = "G-Shop.Application.Reports.SalesReport.rdlc";
+        reportViewer.LocalReport.ReportPath = "Reports/SalesReport.rdlc";
 
-        }
+        SalesReportDataSet salesReportDataSet = new();
+
+        SalesTableAdapter salesTableAdapter = new();
+        salesTableAdapter.Fill(salesReportDataSet.Sales);
+
+        reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSet1", dataSourceValue: salesReportDataSet.Sales.AsEnumerable()));
+        reportViewer.RefreshReport();
     }
 }
